c341ba7921030cde0667e64f0d65677cd5590bb2
Vlad <voronin@surfstudio.ru>
2021-09-24 11:08:32 +0300

    remove unused key

--- lib/res/test_keys.dart
+++ lib/res/test_keys.dart
@@ -4,9 +4,6 @@ abstract class HomeTestKeys {
   /// скролл на экране списка
   static const Key scroll = Key('main_list_scroll');
 
-  /// кнопка добавления/удаление в избранное
-  static Key favoritesToggleBtn(int i) => Key('favorites_toggle_btn_$i');
-
   /// кнопка открытия избранного
   static const Key favoritesBtn = Key('main_favorites_btn');
 }
